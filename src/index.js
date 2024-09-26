f7c680cd3d40ca769e7ab637ee919fcc27ce8939
Josue Granados <20211306@uthh.edu.mx>
2024-09-25 23:13:03 -0600

    nueva actualizacion PWA

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorkerRegistration from './service-worker'; // Importar el service worker
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,10 +11,5 @@ root.render(
   </React.StrictMode>
 );
 
-// Registrar el service worker
-serviceWorkerRegistration.register();
-
 // Si quieres medir el rendimiento en tu app, usa reportWebVitals
 reportWebVitals();
-
-
